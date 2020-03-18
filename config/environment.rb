c0e2635d6554edfb2334233cee6b27f2e95b3730
Bakugou808 <janu.m.sung@gmail.com>
2020-03-17 20:35:18 -0700

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -1,1 +1,2 @@
-# Set up Bundler here following the instructions in the README
+# Set up Bundler here following the instructions in the READMErequire 'bundler/setup'
+Bundler.require(:default, :development)
